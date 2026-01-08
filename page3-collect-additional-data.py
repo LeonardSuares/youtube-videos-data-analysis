f586772989ad44ab10f9075dd9cf35f6a35da156
Leonard <Leonard.suares@gmail.com>
2026-01-08 13:46:18 -0500

    Export data as csv, JSON or a DB file

--- page3-collect-additional-data.py
+++ page3-collect-additional-data.py
@@ -8,11 +8,12 @@ from datetime import datetime, timedelta
 import nltk
 import emoji
 import plotly.graph_objs as go
+from sqlalchemy import create_engine
 from plotly.offline import iplot
 from collections import Counter
 from wordcloud import WordCloud, STOPWORDS
 
-nltk.download('vader_lexicon')
+# nltk.download('vader_lexicon')
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
@@ -38,4 +39,15 @@ for file in files_csv:
     current_df = pd.read_csv(path+'/'+file, encoding='iso-8859-1', on_bad_lines='skip')
     full_df = pd.concat([full_df,current_df], ignore_index=True)
 
+# print(full_df[full_df.duplicated()].shape)
+
+full_df = full_df.drop_duplicates()
+
 print(full_df.shape)
+
+full_df[0:1000].to_csv(r'C:\Users\leona\PycharmProjects\Python Data Analysis Projects\AAProject sets - 2\youtube-videos-data-analysis\export_data/youtube_sample.csv', index = False)
+full_df[0:1000].to_json(r'C:\Users\leona\PycharmProjects\Python Data Analysis Projects\AAProject sets - 2\youtube-videos-data-analysis\export_data/youtube_sample.json')
+
+engine = create_engine(r"sqlite:///C:\Users\leona\PycharmProjects\Python Data Analysis Projects\AAProject sets - 2\youtube-videos-data-analysis\export_data\youtube_sample.sqlite")
+
+full_df[0:1000].to_sql('Users', con=engine, if_exists='append')
